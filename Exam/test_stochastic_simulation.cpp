e17b6c51799e5fb50f12a89e12a0986403385908
mkju19 <mkju19@student.aau.dk>
2023-08-07 20:26:29 +0200

    Added tests for symbol table

--- Exam/test_stochastic_simulation.cpp
+++ Exam/test_stochastic_simulation.cpp
@@ -9,6 +9,7 @@
 #include "Include/Reaction/ReactionComponent.h"
 #include "Include/Reaction/Rule.h"
 #include "Include/Reaction/Reaction.h"
+#include "Include/SymbolTable.h"
 TEST_CASE("Test reactions")
 {
     auto a = Agent("a", 1);
@@ -30,6 +31,48 @@ TEST_CASE("Test reactions")
                         reaction2 << std::endl <<
                         reaction3 << std::endl <<
                         reaction4 << std::endl;
+    }
+
+}
+TEST_CASE("Test Symbol table"){
+    auto a = Agent("a", 1);
+
+    auto symbolTable = SymbolTable<Agent>{};
+
+    SUBCASE("Test that the symbol table can add an object and retrieve it"){
+        auto id1 = "a";
+        auto id2 = "b";
+
+        symbolTable.addElement(id1, a);
+        symbolTable.addElement(id2, a);
+        auto obj = symbolTable.getElement(id1);
+
+        CHECK(obj.getId() == id1);
+        CHECK(obj.getValue() == a.getValue());
+
+    }
+    SUBCASE("Test that Symbol table can update a value"){
+        int newValue = 3;
+
+        symbolTable.addElement(a.getId(), a);
+        symbolTable.updateElement(a.getId(), Agent(a.getId(), newValue));
+
+        auto obj = symbolTable.getElement(a.getId());
+        CHECK(obj.getValue() == newValue);
+    }
+    SUBCASE("Test that symbol table throws an exception when the same ID is added twice"){
+        auto id = "a";
+
+        symbolTable.addElement(id, a);
+        CHECK_THROWS(
+                symbolTable.addElement("a", a)
+        );
+    }
+    SUBCASE("Test that symbol table Throws an exception when getElement is called on an id that does not exist"){
+        CHECK_THROWS(symbolTable.getElement("a"));
+    }
+    SUBCASE("Test that symbol table throws an exception when trying to update an element the does not exist"){
+        CHECK_THROWS(symbolTable.updateElement("a", a));
     }
 
 }
