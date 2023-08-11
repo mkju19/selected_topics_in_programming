b97388c8c3439a77bb04ecb79d7cd08bde0ac07c
mkju19 <mkju19@student.aau.dk>
2023-08-11 13:55:17 +0200

    added benchmarks

---
+++ Exam/benchmark.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+#include <iostream>
+#include <iostream>
+#include "Observers.h"
+#include "benchmark/benchmark.h"
+#include "Examples/SimpleExample.h"
+#include "Examples/CircadianRythmExample.h"
+#include "Examples/CovidExample.h"
+
+using clk = std::chrono::high_resolution_clock;
+using std::chrono::duration;
+using std::milli;
+
+
+static void BM_Simulation(benchmark::State& state){
+    auto observer = SilentCounter{};
+
+    for (auto _ : state){
+        for (auto i = 0; i < 100; i++) {
+            CovidExample::run(observer, state.range());
+        }
+    }
+}
+
+static void BM_ParallelSimulation(benchmark::State& state){
+    auto observer = SilentCounter{};
+    for (auto _ : state){
+            CovidExample::runParallel(observer, 100, state.range());
+    }
+}
+BENCHMARK(BM_Simulation)->Arg(10000);
+BENCHMARK(BM_ParallelSimulation)->Arg(10000);
+
+BENCHMARK_MAIN();
